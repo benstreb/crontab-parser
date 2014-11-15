db41214654c09feaf9351822bd3dbb5c8b06fc3a
Benjamin Streb <Bstreb@gmail.com>
2014-11-15 11:35:33 -0800

    Crontab parser now ignores lines that are comments
    For now invalid entries are accepted. I will probably change that moving
    forward when I start actually parsing crontab entries to try and extract
    necessary information.

--- cronparse.py
+++ cronparse.py
@@ -1,17 +1,27 @@
 """
 This file parses crontabs in order to manipulate them
 >>> from io import StringIO
->>> CronTab(StringIO("VAR=3\\n* * * * * true"))
-CronTab
+>>> CronTab(StringIO("#comment\\n  \t\\nVAR=3\\n* * * * * true"))
+CronTab(jobs=['* * * * * true'])
 """
+
+import re
 
 
 class CronTab:
+
     def __init__(self, file):
-        pass
+        self.jobs = []
+        for line in file:
+            line = line.strip()
+            if (len(line) == 0 or line.startswith("#") or
+                    re.match("\w+\s*=", line)):
+                continue
+            else:
+                self.jobs.append(line)
 
     def __repr__(self):
-        return "CronTab"
+        return "CronTab(jobs="+repr(self.jobs)+")"
 
 if __name__ == "__main__":
     import doctest
